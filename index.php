6bb58ca567d9cbc148078c2cf31b0753a457246b
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2023-12-10 20:28:16 +0600

    Checking all setup

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Abdullah Al Mamun">
+    <meta name="description" content="Full Hospital Management System Design here, also CRUD system include.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hospital MS | Be safe and try to care yourself...</title>
+</head>
+<body>
+    <h1>Main Page</h1>
+</body>
+</html>
