74ab2d674ba9d72e3579ed24a4e30d335074cfbb
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2023-12-10 23:39:19 +0600

    Header design

---
+++ components/header.php
@@ -0,0 +1,34 @@
+<header>
+    <div class="header-container">
+        <div class="header-logo">
+            <a href="#"><span>me</span>HMS</a>
+        </div>
+        <div class="header-nav">
+            <div class="nav-item">
+                <span>Care at <br>Me Hospital</span> 
+                <i class="fa-solid fa-angle-down"></i>
+            </div>
+            <div class="nav-item">
+                <span>Health<br>Library</span> 
+                <i class="fa-solid fa-angle-down"></i>
+            </div>
+            <div class="nav-item">
+                <span>Medial<br>Professionals</span> 
+                <i class="fa-solid fa-angle-down"></i>
+            </div>
+            <div class="nav-item">
+                <span>Research & Edu<br>at meH</span> 
+                <i class="fa-solid fa-angle-down"></i>
+            </div>
+            <div class="nav-item">
+                <span>Giving to<br>meH</span> 
+                <i class="fa-solid fa-angle-down"></i>
+            </div>
+        </div>
+        <div class="header-buttons">
+            <a href="#">Request appointment</a>
+            <a href="#" class="log"><i class="fa-regular fa-user"></i>Log in</a>
+            <i class="fa-solid fa-magnifying-glass header-search"></i>
+        </div>
+    </div>
+</header>
