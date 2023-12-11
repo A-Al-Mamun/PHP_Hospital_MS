19d495cdd03a3528cf745c4bd84efaffed2bee32
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2023-12-11 23:57:26 +0600

    Footer design setup

--- components/footer.php
+++ components/footer.php
@@ -1,5 +1,82 @@
 <footer>
-    <div class="footer-container">
-        <h1>Footer Section</h1>
+    <div class="footer-container1">
+        <div class="footer-btns">
+            <div class="btns-item">
+                <i class="fa-solid fa-user-doctor"></i> <span>Find a doctor</span><i class="fa-solid fa-angle-right"></i>
+            </div>
+            <div class="btns-item">
+                <i class="fa-solid fa-user-doctor"></i> <span>Explore careers</span><i class="fa-solid fa-angle-right"></i>
+            </div>
+            <div class="btns-item">
+                <i class="fa-solid fa-user-doctor"></i> <span>Sign up for free-enewsletters</span><i class="fa-solid fa-angle-right"></i>
+            </div>
+            <h2>Medical Professionals <i class="fa-solid fa-angle-right"></i></h2>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <h2>Medical Professionals <i class="fa-solid fa-angle-right"></i></h2>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+        </div>
+        <div class="footer-professions">
+            <h2>Medical Professionals <i class="fa-solid fa-angle-right"></i></h2>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <h2>Medical Professionals <i class="fa-solid fa-angle-right"></i></h2>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <h2>Medical Professionals <i class="fa-solid fa-angle-right"></i></h2>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <h2>Medical Professionals <i class="fa-solid fa-angle-right"></i></h2>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+        </div>
+        <div class="footer-research">
+            <h2>Researchers <i class="fa-solid fa-angle-right"></i></h2>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <h2>Researchers <i class="fa-solid fa-angle-right"></i></h2>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <h2>Researchers <i class="fa-solid fa-angle-right"></i></h2>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <h2>Researchers <i class="fa-solid fa-angle-right"></i></h2>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+            <a href="#">Title</a>
+        </div>
+    </div>
+    <hr>
+    <div class="footer-container2">
+        <div class="container2-item">
+            <h2>Follow me Hospital</h2>
+            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+            <a href="#"><i class="fa-brands fa-youtube"></i></a>
+            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+        </div>
+        <div class="container2-item">
+            <h2>Get the me Hospital App</h2>
+        </div>
     </div>
 </footer>
